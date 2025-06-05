f9b05e6dc80295e982445c458713786b41defd1e
Andisi Ambuku <66791412+andisiambuku@users.noreply.github.com>
2025-06-06 02:11:29 +0300

    Revert "Add Search, Movie Listing, and Movie Details Features"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,3 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { MovieListComponent } from './features//movie/movie-list/movie-list.component';
-import { MovieDetailsComponent } from './features/movie/movie-details/movie-details.component';
-import { LoginComponent } from './features/login/login.component';
-import { AuthGuard } from './guards/auth.guard';
+import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: '', component: MovieListComponent, canActivate: [AuthGuard],  },
-  { path: 'movie/:id', component: MovieDetailsComponent, canActivate: [AuthGuard],data: { showHeroBanner: false } },
-  { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: '' }
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routes: Routes = [];
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  tmdbApiKey: process.env['TMDB_API_KEY'] as string,
-  tmdbBaseUrl: 'https://api.themoviedb.org/3',
-};
