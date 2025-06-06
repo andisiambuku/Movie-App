d0a233e52851af2d61ef6f3d510cc62a38ac2746
andisiambuku <roselandambuku@gmail.com>
2025-06-06 14:31:36 +0300

    fix: env vbs config for prod

--- replace-env.js
+++ replace-env.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const path = './src/environments/environment.prod.ts';
+const path = './src/environments/environment.ts';
 
 // Read the file
 let content = fs.readFileSync(path, 'utf8');
