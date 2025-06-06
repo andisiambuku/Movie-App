a64eb860fe868a9e5c897441f4f791db6d50919f
andisiambuku <roselandambuku@gmail.com>
2025-06-06 14:30:08 +0300

    fix: env vbs config for prod

---
+++ replace-env.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = './src/environments/environment.prod.ts';
+
+// Read the file
+let content = fs.readFileSync(path, 'utf8');
+
+// Replace the placeholder with the actual value
+content = content.replace(
+  /tmdbApiKey: 'TMDB_API_KEY_PLACEHOLDER'/,
+  `tmdbApiKey: '${process.env.TMDB_API_KEY}'`
+);
+
+// Write back the file
+fs.writeFileSync(path, content, 'utf8');
